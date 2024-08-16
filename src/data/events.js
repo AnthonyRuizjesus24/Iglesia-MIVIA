d24f2ee1f5fa6b8ff90dcdce1224fc120f46af6d
Max Ruiz <maxruizjesus40@gmail.com>
2024-08-16 13:05:47 -0500

    se creo paginas y se dio diseño a algunas paginas

---
+++ src/data/events.js
@@ -0,0 +1,10 @@
+export const events = [
+    {
+      id: 1,
+      title: 'Evento de Ejemplo 1',
+      date: '2024-08-20',
+      description: 'Descripción del evento de ejemplo 1.',
+    },
+    // Agrega más eventos aquí
+  ];
+  
---
+++ src/data/ministries.js
@@ -0,0 +1,9 @@
+export const ministries = [
+    {
+      id: 1,
+      name: 'Ministerio de Ejemplo 1',
+      description: 'Descripción del ministerio de ejemplo 1.',
+    },
+    // Agrega más ministerios aquí
+  ];
+  
